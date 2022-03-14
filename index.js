afc1828c4faeebe56af8321c53277ecb7e5cef60
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-03-14 13:19:37 +0500

    web7

--- index.js
+++ index.js
@@ -8,19 +8,19 @@
 //     alert(`${ism} siz kirishingiz mumkin emas`);
 // }
 
-let son1 = prompt('Son1 ni kiriting')
-let son2 = prompt("son2 ni kiriting")
-let natija = son1 + son2
+// let son1 = prompt('Son1 ni kiriting')
+// let son2 = prompt("son2 ni kiriting")
+// let natija = son1 + son2
 
-alert(`${son1}dan ${son2}ni ayirsak javob ${natija} bo'ladi`)
-natija % 2 == 0
-natija % 2 > 0
+// alert(`${son1}dan ${son2}ni ayirsak javob ${natija} bo'ladi`)
+// natija % 2 == 0
+// natija % 2 > 0
 
-if("natija 2ga qoldiqsiz bo`linsa"){
-    // natija 2ga qoldiqsiz bo`linadi 
-}else if ("natija 2ga qoldiqli bo`linsa "){
-    //natija 2 ga qoldiqli bo`linadi 
-}
+// if("natija 2ga qoldiqsiz bo`linsa"){
+//     // natija 2ga qoldiqsiz bo`linadi 
+// }else if ("natija 2ga qoldiqli bo`linsa "){
+//     //natija 2 ga qoldiqli bo`linadi 
+// }
 
 
 
