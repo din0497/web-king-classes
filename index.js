4dd21db358cdcc23224401da131ade338a0882ed
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-03-14 09:58:52 +0500

    web 3 dars

---
+++ index.js
@@ -0,0 +1,72 @@
+// let ism = prompt('Ismingiz nima?') // Behruz
+// let yosh =prompt('Necha yoshdasiz?') // 13
+
+// if(yosh >= 10 ){
+//     alert(`Marxamat ${ism} siz kirishingiz mumkin!`);
+// }
+// else if (yosh < 10){
+//     alert(`${ism} siz kirishingiz mumkin emas`);
+// }
+
+let son1 = prompt('Son1 ni kiriting')
+let son2 = prompt("son2 ni kiriting")
+let natija = son1 + son2
+
+alert(`${son1}dan ${son2}ni ayirsak javob ${natija} bo'ladi`)
+natija % 2 == 0
+natija % 2 > 0
+
+if("natija 2ga qoldiqsiz bo`linsa"){
+    // natija 2ga qoldiqsiz bo`linadi 
+}else if ("natija 2ga qoldiqli bo`linsa "){
+    //natija 2 ga qoldiqli bo`linadi 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ism = prompt('Ismingiz nima?');
+// let yosh = prompt("yoshingiz nechida");
+// let joy = prompt("qayerda tug`ulgansiz?");
+
+// alert(`Rahmat ${ism}! console orqali ko'rishingiz mn!`)
+
+// let matn = "Men " + ism + "man yoshim " + yosh + " da " + joy + "da tug`ulganman"
+
+// let matn = `Men ${ism}man, yoshim ${yosh}da. Men ${joy}da tug'ulganman.`;
+
+// console.log(matn);
+
+// let food = prompt("Mushuk qorni och");
+
+// if (food == "shashlik" || food == "sut" || food == "gosht" || food == "baliq") {
+//   alert(`Mushuk ${food} yeydi`);
+// } else if (
+//   food != "shashlik" ||
+//   food != "sut" ||
+//   food != "gosht" ||
+//   food != "baliq"
+// ) {
+//   alert(`Mushuk ${food} yemaydi`);
+// }
