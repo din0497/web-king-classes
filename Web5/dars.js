a54bbe97e0d2aba925af6d144aad2610672bbc6a
Rivojiddin Abdunazarov <mr.rivojidin@gmail.com>
2022-04-03 11:06:39 +0500

    smt

---
+++ Web5/dars.js
@@ -0,0 +1,10 @@
+// let Jumagul = 18
+// let rang = 'qizil'
+// Jumagul = rang
+// console.log(Jumagul);
+
+
+let a = 15
+a = 16
+a= 18
+console.log(a);// 18
--- index.js
+++ index.js
@@ -1,90 +1,16 @@
-// function result() {
-//   let age = 6;
-//   let age1 = 5;
-//   let natija = age + age1;
-//   console.log(natija);
-// }
+const like = document.getElementById("like");
+const dislike = document.getElementById("dislike");
 
-// result()
-// let ism = prompt('Ismingiz nima?');
-// let kiritilganYil = prompt('Tug`ulgan yilingiz?')
+const korinishLike = document.getElementById("display1");
+const korinishDislike = document.getElementById("display2");
 
-// function yoshniHisoblang (name, kiritilgan, hozirgiYil) {
-//     let yosh = hozirgiYil - kiritilgan;
-//     alert(`Salom ${name}, Siz ${yosh}yoshdasiz!`)
-// }
+const getLike = () => {
+  ++korinishLike.innerText;
+};
 
-// yoshniHisoblang (ism,kiritilganYil, 2022 )
+const getDislike = () => {
+  korinishDislike= ++korinishDislike.innerText;
+};
 
-//const img = document.querySelector('img')
-// const text = document.querySelector('h1');
-// const like = document.querySelector('.like')
-// const dislike = document.querySelector('.dislike')
-// let likelarSoni = 0;
-
-// function likeniQosh () {
-//     likelarSoni ++
-//     text.textContent = likelarSoni;
-//     img.style.left=`${likelarSoni}px`
-//     if(likelarSoni > 0){
-//         text.style.color = 'green'
-//     }
-// }
-
-// function likeniAyir () {
-//     likelarSoni--
-//     text.textContent = likelarSoni;
-//     img.style.right=`${likelarSoni}px`
-
-//     if(likelarSoni < 0){
-//         text.style.color = 'red'
-//     }
-// }
-
-//  like.addEventListener('click', likeniQosh)
-//  dislike.addEventListener('click', likeniAyir)
-function tur (param) {
-    return typeof param
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let ism = prompt('Ismingiz nima?');
-// let yosh = prompt("yoshingiz nechida");
-// let joy = prompt("qayerda tug`ulgansiz?");
-
-// alert(`Rahmat ${ism}! console orqali ko'rishingiz mn!`)
-
-// let matn = "Men " + ism + "man yoshim " + yosh + " da " + joy + "da tug`ulganman"
-
-// let matn = `Men ${ism}man, yoshim ${yosh}da. Men ${joy}da tug'ulganman.`;
-
-// console.log(matn);
-
-// let food = prompt("Mushuk qorni och");
-
-// if (food == "shashlik" || food == "sut" || food == "gosht" || food == "baliq") {
-//   alert(`Mushuk ${food} yeydi`);
-// } else if (
-//   food != "shashlik" ||
-//   food != "sut" ||
-//   food != "gosht" ||
-//   food != "baliq"
-// ) {
-//   alert(`Mushuk ${food} yemaydi`);
-// }
+like.addEventListener("click", getLike);
+dislike.addEventListener('click', getDislike)
